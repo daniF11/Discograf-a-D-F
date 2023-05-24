dbbbe44d8201e7f5e761a77db11a79e660d591f1
Daniel F. Rincón <danyforce11@hotmail.com>
2023-05-24 18:22:19 -0500

    Se crea frontend de la página

---
+++ js/codigo.js
@@ -0,0 +1,109 @@
+const album = document.querySelectorAll('.img-album');
+const album1 = document.getElementById('album1');
+const album2 = document.getElementById('album2');
+const album3 = document.getElementById('album3');
+const album4 = document.getElementById('album4');
+const album5 = document.getElementById('album5');
+const album6 = document.getElementById('album6');
+const album7 = document.getElementById('album7');
+const album8 = document.getElementById('album8');
+const album9 = document.getElementById('album9');
+
+album1.addEventListener('click', ()=>{
+	function abrirNuevoTab(url) {
+        // Abrir nuevo tab
+        let win = window.open(url, '_blank');
+        // Cambiar el foco al nuevo tab (punto opcional)
+        win.focus();
+    }
+
+    abrirNuevoTab('../albums/votd.html')
+    
+});
+
+album2.addEventListener('click', ()=>{
+	function abrirNuevoTab(url) {
+        // Abrir nuevo tab
+        let win = window.open(url, '_blank');
+        // Cambiar el foco al nuevo tab (punto opcional)
+        win.focus();
+    }
+    abrirNuevoTab('../albums/sf.html')
+});
+
+album3.addEventListener('click', ()=>{
+	function abrirNuevoTab(url) {
+        // Abrir nuevo tab
+        let win = window.open(url, '_blank');
+        // Cambiar el foco al nuevo tab (punto opcional)
+        win.focus();
+    }
+    abrirNuevoTab('../albums/ir.html')
+});
+
+album4.addEventListener('click', ()=>{
+    function abrirNuevoTab(url) {
+        // Abrir nuevo tab
+        let win = window.open(url, '_blank');
+        // Cambiar el foco al nuevo tab (punto opcional)
+        win.focus();
+    }
+    abrirNuevoTab('../albums/ub.html')
+});
+
+album5.addEventListener('click', ()=>{
+    function abrirNuevoTab(url) {
+        // Abrir nuevo tab
+        let win = window.open(url, '_blank');
+        // Cambiar el foco al nuevo tab (punto opcional)
+        win.focus();
+    }
+    abrirNuevoTab('../albums/td.html')
+});
+
+album6.addEventListener('click', ()=>{
+    function abrirNuevoTab(url) {
+        // Abrir nuevo tab
+        let win = window.open(url, '_blank');
+        // Cambiar el foco al nuevo tab (punto opcional)
+        win.focus();
+    }
+    abrirNuevoTab('../albums/tpw.html')
+});
+
+
+album7.addEventListener('click', ()=>{
+    function abrirNuevoTab(url) {
+        // Abrir nuevo tab
+        let win = window.open(url, '_blank');
+        // Cambiar el foco al nuevo tab (punto opcional)
+        win.focus();
+    }
+    abrirNuevoTab('../albums/mo.html')
+});
+
+album8.addEventListener('click', ()=>{
+    function abrirNuevoTab(url) {
+        // Abrir nuevo tab
+        let win = window.open(url, '_blank');
+        // Cambiar el foco al nuevo tab (punto opcional)
+        win.focus();
+    }
+    abrirNuevoTab('../albums/rii.html')
+});
+
+album9.addEventListener('click', ()=>{
+    function abrirNuevoTab(url) {
+        // Abrir nuevo tab
+        let win = window.open(url, '_blank');
+        // Cambiar el foco al nuevo tab (punto opcional)
+        win.focus();
+    }
+    abrirNuevoTab('../albums/epm.html')
+});
+
+
+
+
+
+
